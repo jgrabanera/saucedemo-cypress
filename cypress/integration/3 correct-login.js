536f8d25eb5c4d355afaada10f6aa723db7da2d1
jgrabanera <jgrabanera@gmail.com>
2022-06-27 03:51:29 +0800

    comment

---
+++ cypress/integration/3 correct-login.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+//import { constants } from "../../../support/constants" //not working
+
+describe("Test Login Form via saucedemo", () => {
+  it("Login correct email and password", () => {
+    //visit saucedemo website
+    cy.visit("https://www.saucedemo.com/")
+
+    cy.get("[data-test=username]").type("standard_user")
+    cy.get("[data-test=username]").should("have.attr", "name", "user-name") //validate username input
+
+    cy.get("[data-test=password]").type("secret_sauce")
+    cy.get("[data-test=password]").should("have.attr", "name", "password") //validate password input
+
+    cy.get("[data-test=login-button]").click()
+
+    cy.url().should("include", "/inventory") //validate page url
+    cy.log("Successfully Login")
+  })
+
+})
